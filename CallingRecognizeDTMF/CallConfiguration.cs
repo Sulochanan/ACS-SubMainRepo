e0798a6910dffc88a7bfad2c15d0b6d3ca614289
maulinasharma <v-mausha@microsoft.com>
2022-09-09 23:53:22 +0530

    VSO #2904872: [C#][Sample] Server Calling Content -  Recognize DTMF sample

---
+++ CallingRecognizeDTMF/CallConfiguration.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace CallingRecognizeDTMF
+{
+    /// <summary>
+    /// Configuration assoociated with the call.
+    /// </summary>
+    public class CallConfiguration
+    {
+        public CallConfiguration(string connectionString, string sourceIdentity, string sourcePhoneNumber, 
+            string appBaseUrl, string audioFileName, string salesAudioFileName, string marketingAudioFileName, string customerCareAudioFileName)
+        {
+            this.ConnectionString = connectionString;
+            this.SourceIdentity = sourceIdentity;
+            this.SourcePhoneNumber = sourcePhoneNumber;
+            this.AppBaseUrl = appBaseUrl;
+            this.AudioFileName = audioFileName;
+            this.SalesAudioFileName = salesAudioFileName;
+            this.MarketingAudioFileName = marketingAudioFileName;
+            this.CustomerCareAudioFileName = customerCareAudioFileName;
+        }
+
+        /// <summary>
+        /// The connectionstring of Azure Communication Service resource.
+        /// </summary>
+        public string ConnectionString { get; private set; }
+
+        /// <summary>
+        /// The source identity.
+        /// </summary>
+        public string SourceIdentity { get; private set; }
+
+        /// <summary>
+        /// The phone number associated with the source. 
+        /// </summary>
+        public string SourcePhoneNumber { get; private set; }
+
+        /// <summary>
+        /// The base url of the applicaiton.
+        /// </summary>
+        private string AppBaseUrl;
+
+        /// <summary>
+        /// The audio file name of the play prompt.
+        /// </summary>
+        public string AudioFileName;
+
+        /// <summary>
+        /// The audio file name of the audio when input is 1.
+        /// </summary>
+        public string SalesAudioFileName;
+
+        /// <summary>
+        /// The audio file name of the play prompt.
+        /// </summary>
+        public string MarketingAudioFileName;
+
+        /// <summary>
+        /// The audio file name of the play prompt.
+        /// </summary>
+        public string CustomerCareAudioFileName;
+
+        /// <summary>
+        /// The callback url of the application where notification would be received.
+        /// </summary>
+        public string AppCallbackUrl => $"{AppBaseUrl}/api/outboundcall/callback?{EventAuthHandler.GetSecretQuerystring}";
+
+        /// <summary>
+        /// The publicly available url of the audio file which would be played as a prompt.
+        /// </summary>
+        public string AudioFileUrl => $"{AppBaseUrl}/audio/";
+    }
+}
